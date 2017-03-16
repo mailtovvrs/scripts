7b44d36ec90fe6c6ab31130d8402100fc21ae03e
mailtovvrs <mailtovvrs@gmail.com>
2017-03-16 11:46:37 -0700

    Added comments to the script

--- hbck_check_fix.sh
+++ hbck_check_fix.sh
@@ -8,10 +8,11 @@ PATH="/usr/lib64/qt-3.3/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:
 check()
 {
 
-# Checking full report of all regions
+# Checking detailed report of all regions(tables) in HBase
 
 runuser -l $USER_TO_RUN -c '/usr/bin/hbase hbck -details' > /dev/null 2>&1 >  /root/scripts/HBase_ConsistencyReport
 
+# From the redirected file:HBase_ConsistencyReport grep for Status: OK
 cat HBase_ConsistencyReport | grep -i "Status: OK"  > /dev/null 2>&1
 
        if [ $? -eq 0 ]
@@ -19,11 +20,17 @@ cat HBase_ConsistencyReport | grep -i "Status: OK"  > /dev/null 2>&1
        echo "HBase Cluster Consistency check - [HEALTHY]"
        exit 0
        else
+  
+  # If there are any inconsistencies detected in the tables sending email with appending the file content  
   echo "HBase Cluster Consistency check[DETAILED] - [UNHEALTHY] $(cat HBASEREGIONSREPORT)" | mailx -s "HBASE UNHEALTHY - Action required" -r "DoNotReply" $mailto
+        
+        # Once if there are any inconsistencies detected the following funtion would be called to fix(/usr/bin/hbase hbck -fix) the inconsistencies
         fix()
         {
          runuser -l $USER_TO_RUN -c '/usr/bin/hbase hbck -fix' > /dev/null 2>&1 > /root/scripts/HBase_ConsistencyFixReport
          cat HBase_ConsistencyFixReport | grep -i "Status: OK"  > /dev/null 2>&1
+         
+         # Checking to ensure if after running the /usr/bin/hbase hbck -fix the inconsistencies are fixed from the file:HBase_ConsistencyFixReport
           if [ $? -eq 0 ]
           then
           echo "HBase Cluster Consistencies Fixed - [FIXED] $(cat HBase_ConsistencyFixReport)" | mailx -s "HBASE UNHEALTHY IS FIXED - No Action Required" -r "DoNotReply" $mailto
