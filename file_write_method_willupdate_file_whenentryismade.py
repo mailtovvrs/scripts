5bb4dbe80c2dd644c43d76b9c29dbf545b5f8bf7
mailtovvrs <mailtovvrs@gmail.com>
2017-08-27 20:15:43 -0700

    Update file_write_method_willupdate_file_whenentryismade.py

--- file_write_method_willupdate_file_whenentryismade.py
+++ file_write_method_willupdate_file_whenentryismade.py
@@ -6,3 +6,13 @@ performances = {'Ventriloquism':       '9:00am',
 schedule_file = open('schedule.txt','w')
 for key,val in performances.items():
     schedule_file.write(key + '-' + val + '\n')
+
+    ==============
+    OUTPUT:cat schedule.txt 
+Amazing Acrobatics-2:00pm
+Venkat212-6:00pm
+Enchanted Elephants-5:00pm
+Ventriloquism-9:00am
+Snake Charmer-12:00pm
+  
+      
