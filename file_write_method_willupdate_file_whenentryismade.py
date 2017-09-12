1c44e99b64f8a255628caee2b78b2e7e3e209582
Venkat Velagala <venkat.velagala@infor.com>
2017-09-12 09:44:52 -0700

    Adding Custom Ambari Alert for Region Server File Locality in HBase

--- file_write_method_willupdate_file_whenentryismade.py
+++ file_write_method_willupdate_file_whenentryismade.py
@@ -1,7 +1,7 @@
 performances = {'Ventriloquism':       '9:00am', 
                 'Snake Charmer':       '12:00pm', 
                 'Amazing Acrobatics':  '2:00pm', 
-                'Venkat21':	       '6:00pm',
+                'Venkat21245':	       '6:00pm',
                 'Enchanted Elephants': '5:00pm'}
 schedule_file = open('schedule.txt','w')
 for key,val in performances.items():
