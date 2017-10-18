b827004654d25d981514aa8a93c34d663bf9dd6c
mailtovvrs <mailtovvrs@gmail.com>
2017-10-18 13:06:10 -0700

    Update ambaribackup_mountpointcheck.sh

--- ambaribackup_mountpointcheck.sh
+++ ambaribackup_mountpointcheck.sh
@@ -4,11 +4,12 @@ POSTGRESDB_BACKUP="/backup/hadoop/DD/`date +%Y-%m-%d`/ambari"
 POSTGRES_BACKEDFILE="/backup/hadoop/DD/`date +%Y-%m-%d`/ambari/`hostname -f`_ambari_metadata_postgres_backup_`/bin/date +\%Y\%m\%d`.sql"
 POSTGRES_USER="postgres"
 DELETEFILES="/backup/hadoop/DD/"
-#mailto=_data_operations@infor.com
-mailto=venkat.velagala@infor.com
+mailto=xxxxx
+
 # Create the backup directory
 mkdir -p $POSTGRESDB_BACKUP
 PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/opt/TradeCard/bin:/root/bin;/opt/dataops/scripts/ambaribackup_dd.sh;"
+
 # Backup command
 if  mountpoint -q /backup/hadoop/;
 then
@@ -17,11 +18,13 @@ else
 echo "You can't write to root"
 exit 1
 fi
+
 if  [  "$(grep -i 'PostgreSQL database dump complete' $POSTGRES_BACKEDFILE)" ] && mountpoint -q /backup/hadoop;
 then
 echo "Success"
 else
 echo "Dataops Team please taken an action: Ambari Postgres backup failed due to the reason that the string PostgreSQL database completed was not found on `hostname` at `date` in $POSTGRES_BACKEDFILE"|  mailx -s "dataops@gtnexus.com" -s "FAILURE: Ambari Postgres Backup failed on `hostname -f`" -S smtp="mail.dc.gtnexus.com:25" -S ssl-verify=ignore $mailto
 fi
+
 # Remove backups older than 14 days
 find $DELETEFILES -maxdepth 1 -type d -mtime +14 -exec rm -rf {} \;
