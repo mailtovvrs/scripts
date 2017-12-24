0a0ae4c0d6041c6420009d891e1b3db29c45dc8f
mailtovvrs <mailtovvrs@gmail.com>
2017-12-23 16:00:34 -0800

    Update hbck_check_fix.sh

--- hbck_check_fix.sh
+++ hbck_check_fix.sh
@@ -3,7 +3,6 @@ USER_TO_RUN="hbase"
 HBCKSTATUS="/root/scripts/HBCKSTATUS"
 mailto="xxxxxx"
 
-PATH="/usr/lib64/qt-3.3/bin:/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/opt/java/jdk1.7.0_67/bin:/opt/TradeCard/bin:/root/bin;/root/scripts/hbasesh.sh;"
 
 check()
 {
